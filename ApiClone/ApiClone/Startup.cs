1e78bfb45d5e82a2566c93c62a966286744b9a9a
Abdujalilc <abdujalilc@gmail.com>
2021-12-30 11:01:53 +0500

    some changes

--- ApiClone/ApiClone/Startup.cs
+++ ApiClone/ApiClone/Startup.cs
@@ -23,7 +23,7 @@ namespace ApiClone
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper();
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
